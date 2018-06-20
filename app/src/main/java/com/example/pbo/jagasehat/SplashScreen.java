6da17c9af9292917b42188685c75b986b765eb0c
faizinahsan <faizin110998@gmail.com>
2018-06-20 07:45:55 +0700

    Activity FormDemografi cuman jalan sekali

--- app/src/main/java/com/example/pbo/jagasehat/SplashScreen.java
+++ app/src/main/java/com/example/pbo/jagasehat/SplashScreen.java
@@ -6,16 +6,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        Boolean isFirstRun = getSharedPreferences("PREFERENCE", MODE_PRIVATE)
+                .getBoolean("isFirstRun", true);
+        //to run activity just once after the app is installed
+        if (isFirstRun) {
+            //show sign up activity
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    startActivity(new Intent(SplashScreen.this,FormDemografi.class));
+                    finish();
+                }
+            },3000);
+        }
+
+
+        getSharedPreferences("PREFERENCE", MODE_PRIVATE).edit()
+                .putBoolean("isFirstRun", false).commit();
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashScreen.this,FormDemografi.class));
+                startActivity(new Intent(SplashScreen.this,MainActivity.class));
                 finish();
             }
         },3000);
